e3d75fef420c1ca17cd2c88687953ba7bb69d5c6
Aaryan Tiwary <75285979+atiwaryandroid@users.noreply.github.com>
2023-09-19 01:47:54 -0700

    Delete newMap.py

--- newMap.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-def newMap(target, i):
-    picName = "TestRun" + str(i) + ".png"
-    plt.imshow(target)
-    plt.title("Test Run " + str(i))
-    plt.savefig(picName)
